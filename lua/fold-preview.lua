6e1ec7d72316ebb90f688bc570be45caba768066
Jaekyeom Kim <btapiz@gmail.com>
2024-07-28 15:28:54 -0400

    Remove `eventignore` settings.

--- lua/fold-preview.lua
+++ lua/fold-preview.lua
@@ -195,12 +195,10 @@ function M.show_preview()
       focusable = false,
       noautocmd = true
    })
-   o.eventignore = 'all'
    wo[winid].winhighlight = 'NormalFloat:FoldPreview,FloatBorder:FoldPreviewBorder'
    wo[winid].foldenable = false
    wo[winid].signcolumn = 'no'
    wo[winid].conceallevel = wo[curwin].conceallevel
-   o.eventignore = nil
 
    function M._close_preview()
       if api.nvim_win_is_valid(winid) then
